1496681835f27daa358221e6fb484fc70afef72f
mBalao9 <jessi.hane@gmail.com>
2024-11-05 14:50:16 -0300

    chore: add shadcn setup

--- finance.ai/app/page.tsx
+++ finance.ai/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex w-screen items-center justify-center">
       <h1 className="p-5 text-red-500">hello next</h1>
+
+      <Button>Clique aqui</Button>
     </div>
   );
 }
