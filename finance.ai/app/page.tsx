a55d9b4d1c02f7a8b81585635ce067c23680469f
mBalao9 <jessi.hane@gmail.com>
2024-11-05 16:34:58 -0300

    feat: add authentication with clerk

--- finance.ai/app/page.tsx
+++ finance.ai/app/page.tsx
@@ -1,11 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./_components/ui/button";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
   return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="p-5 text-red-500">hello next</h1>
-
-      <Button>Clique aqui</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
     </div>
   );
-}
+};
+
+export default Home;
